f185e64ff676a83df4615adaa0eaa8168293a793
YuqianXie <yuqianxie2000@gmail.com>
2020-11-04 22:26:53 +0800

    modified slide capture system

--- slide_capture.py
+++ slide_capture.py
@@ -1,24 +1,23 @@
 import numpy as np
 import cv2
 
-# Capture video from camera
-cap = cv2.VideoCapture(0)
+def slideCapture(videofile):
 
-# or playing video from file
-# cap = cv2.VideoCapture('...')
+    # Playing video from file
+    cap = cv2.VideoCapture(videofile)
 
-while(True): # cap.isOpened()
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    while(cap.isOpened()): 
+        # Capture frame-by-frame
+        ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
